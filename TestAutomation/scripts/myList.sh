f1ae2c40b5172df9717a9adda1729c7fcdddbc78
priesterbd <priesterbd@g.cofc.edu>
2019-09-10 09:37:11 -0400

    Update commit

--- TestAutomation/scripts/myList.sh
+++ TestAutomation/scripts/myList.sh
@@ -1,11 +1,17 @@
-#!/bin/bash
-#lists top level (root directory) contents
+#/bin/bash
+#script lists contents of top-level directory
+#Evan Tanner, Brandon Priester, Cameron Dey
 
 cd ..
-contents = ls -a
+ls | cat > tempContents.txt
+sed '/tempContents.txt/d' tempContents.txt > contents.txt
+rm tempContents.txt
 
-htmlTest= <!DOCTYPE html><html><body><h1>My First Heading</h1><p>$contents</p></body></html>
+echo '<!DOCTYPE html><html><body><h1>Root Files Listed</h1><p>' > webHtml.html 
+cat contents.txt >> webHtml.html
+echo '</p></body></html>' >> webHtml.html
 
+rm contents.txt
+firefox webHtml.html
+rm webHtml.html
 
-
-firefox $htmlTest
