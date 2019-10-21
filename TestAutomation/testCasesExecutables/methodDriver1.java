6c891dbe618a8ea95a11f3ebe33db6df2441e74d
priesterbd <priesterbd@g.cofc.edu>
2019-10-21 02:27:50 -0400

    Fixed import from jar

--- TestAutomation/testCasesExecutables/methodDriver1.java
+++ TestAutomation/testCasesExecutables/methodDriver1.java
@@ -1,22 +1,37 @@
-import ;
+import org.openmrs.util.OpenmrsUtil;
 
-public class methodDriver1
-{
+public class methodDriver1 {
 	
-	public static void main(String args[])
-	{
+	public static void main(String args[]) {
 		
 		System.out.println("Hello World!");
-		System.out.print("Your input is: ");
-
-		for(int i=0; i<args.length; i++)
+		
+		
+		if (args.length>=1) 
 		{
-			
-			System.out.print(args[i] + " ");
+			System.out.print("Your input is: ");
+		
+			for (int i = 0; i < args.length; i++) {
+				
+				System.out.print(args[i] + " ");
+				
+			}
 
+			System.out.println();
+			System.out.print("Your output is: ");
+
+			int output;			
+
+			output = OpenmrsUtil.convertToInteger(Long.parseLong(args[0]));
+			System.out.println(output);
+		
 		}
+		else
+		{
 
-		System.out.println();
+			System.out.println("You did not provide an input");
+
+		}		
 	}
-
+	
 }
