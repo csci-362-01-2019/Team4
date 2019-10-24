eb748283488e17b9cf99c4e7368fa5d67f88bfbb
priesterbd <priesterbd@g.cofc.edu>
2019-10-24 12:20:24 -0400

    Fixed bug in runAllTests.sh

--- TestAutomation/scripts/runAllTests.sh
+++ TestAutomation/scripts/runAllTests.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+rm -rf ./oracles/*
+rm -rf ./temp/*
+
+
 
 for file in testCases/*.txt; do	
 	TESTID=$(sed -n '1p' $file)
-	sed -n '1p' $file > ./oracles/testCaseOracle$TESTID.txt
+	sed -n '5p' $file > ./oracles/testCaseOracle$TESTID.txt
 done
 
 #./scripts/subDrivers/subDriver1.sh
