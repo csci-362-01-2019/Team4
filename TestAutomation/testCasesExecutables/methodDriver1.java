e6baaf323cb4644ea17c12cf209ac523ffacc9bb
deyc <deyc@g.cofc.edu>
2019-10-23 21:04:36 -0400

    methodDriver1.class

--- TestAutomation/testCasesExecutables/methodDriver1.java
+++ TestAutomation/testCasesExecutables/methodDriver1.java
@@ -2,20 +2,8 @@ import org.openmrs.util.OpenmrsUtil;
 
 public class methodDriver1 {
 	
-	public static void main(String args[]) {
-		
-		System.out.println("Hello World!");
-		
-		System.out.print("Your input is: ");
-	
-		for (int i = 0; i < args.length; i++) {
-			
-			System.out.print(args[i] + " ");
-			
-		}
-
-		System.out.println();
-		System.out.print("Your output is: ");
+	public static void main(String args[]) 
+	{
 
 		long input;
 		String output;
