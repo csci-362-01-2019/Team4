1170d18132291201687a27afc5d79a5fc3e97688
deyc <deyc@g.cofc.edu>
2019-10-28 12:18:45 -0400

    Updated Driver

--- TestAutomation/scripts/subDrivers/subDriver1.sh
+++ TestAutomation/scripts/subDrivers/subDriver1.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
-VAR1="1"
-VAR2="/testCases/testCase$VAR1.txt"
+# Going to the root file!
+cd ..
+cd ..
+# echo $PWD
+
+VAR1="1"									
+VAR2="/testCases/testCase$VAR1.txt"			
 sed -n 3p .$VAR2
+
+
+# Compiling the Java file!
+cd ./testCasesExecutables/
+javac -cp openmrs-api.jar: methodDriver1.java
+
+
+#Executing the Java file!
+#This needs to do a loop through the testcases
+#and see which ones it to test by method name
+inputVariable="0"
+java -cp openmrs-api.jar: methodDriver1 $inputVariable
+
+
+#Going back to the original directory
+cd ..
+cd ./scripts/subDrivers
