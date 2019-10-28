92e2b978bf18f6a925ac37e8fd66b0f4421eb678
deyc <deyc@g.cofc.edu>
2019-10-28 12:48:06 -0400

    Functions with main script

--- TestAutomation/scripts/subDrivers/subDriver1.sh
+++ TestAutomation/scripts/subDrivers/subDriver1.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
 
+
+#NOT NEEDED IF RAN FROM MAIN SCRIPT!!!!!
 # Going to the root file!
-cd ..
-cd ..
-# echo $PWD
+#echo $PWD
+#cd ..
+#echo $PWD
+#cd ..
+#echo $PWD
+
+# Compiling the Java file!
+cd ./testCasesExecutables/
+javac -cp openmrs-api.jar: methodDriver1.java
+
+#back to root
+cd .. 
 
 VAR1="1"									
 VAR2="/testCases/testCase$VAR1.txt"			
 sed -n 3p .$VAR2
 
 
-# Compiling the Java file!
-cd ./testCasesExecutables/
-javac -cp openmrs-api.jar: methodDriver1.java
-
-
 #Executing the Java file!
 #This needs to do a loop through the testcases
 #and see which ones it to test by method name
+cd ./testCasesExecutables/
 inputVariable="0"
 java -cp openmrs-api.jar: methodDriver1 $inputVariable
 
