72ef0f15890be640084e09187639c38e6177cc2a
priesterbd <priesterbd@g.cofc.edu>
2019-10-28 22:34:49 -0400

    Finalized drivers

--- TestAutomation/scripts/runAllTests.sh
+++ TestAutomation/scripts/runAllTests.sh
@@ -1,21 +1,37 @@
 #!/bin/bash
 
+#removes all files
 rm -rf ./oracles/*
 rm -rf ./temp/*
-echo "..." > ./temp/DummyText.txt
+echo "..." > ./temp/DummyText
+echo "..." > ./oracles/DummyText
 
 
-
+#getting the oracles from file
 for file in testCases/*.txt; do	
 	TESTID=$(sed -n '1p' $file)
-	sed -n '5p' $file > ./oracles/testCaseOracle$TESTID.txt
+	sed -n '5p' $file > ./oracles/testOracle$TESTID.txt
 done
 
-
+#running subdrivers (getting I/O results)
 for file in scripts/subDrivers/*.sh; do
 	./$file
 done
-#./scripts/subDrivers/subDriver1.sh
-#./scripts/subDrivers/subDriver2.sh
-#./scripts/subDrivers/subDriver3.sh
 
+#Compares oracles to results
+count=0
+for file in oracles/*.txt; do
+	count=$((count+1))
+  realOut=$(cat ./temp/outputTest$count.txt)
+  realIn=$(sed -n '4p' ./testCases/testCase$count.txt)
+  oracle=$(cat $file)
+  
+  result="Fail"
+  if [ "$realOut" == "$oracle" ]
+  then
+  	result="Pass"
+  fi
+  
+  echo -e "<pre>Test $count\tInput: $realIn\tOracle: $oracle\tResult: $result</pre>" >> ./reports/finalReport.txt
+  
+done
