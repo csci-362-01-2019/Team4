5fe7b25bd9fdd5053a0cafea838cdb66d657ec23
deyc <deyc@g.cofc.edu>
2019-10-28 21:53:14 -0400

    Now outputs To Files

--- TestAutomation/scripts/subDrivers/subDriver1.sh
+++ TestAutomation/scripts/subDrivers/subDriver1.sh
@@ -16,19 +16,28 @@ javac -cp openmrs-api.jar: methodDriver1.java
 #back to root
 cd .. 
 
-VAR1="1"									
-VAR2="/testCases/testCase$VAR1.txt"			
-sed -n 3p .$VAR2
-
-
 #Executing the Java file!
 #This needs to do a loop through the testcases
 #and see which ones it to test by method name
-cd ./testCasesExecutables/
-inputVariable="0"
-java -cp openmrs-api.jar: methodDriver1 $inputVariable
 
 
-#Going back to the original directory
-cd ..
-cd ./scripts/subDrivers
+for file in ./testCases/*.txt; do
+	temp=$(sed -n 3p $file)
+	if [ "$temp" == "$method" ]
+	then
+		inputVariable=$(sed -n 4p $file)
+    testID=$(sed -n 1p $file)
+    
+    cd ./testCasesExecutables/
+		output=$(java -cp openmrs-api.jar: methodDriver1 $inputVariable)
+    cd ..
+    echo $output > ./temp/outputTest$testID.txt
+    
+  fi
+done
+
+
+
+
+
+
