d38a8023b5158fe1732b1496aabb00ac0b9a1db9
zero <surfevant@gmail.com>
2019-11-07 18:28:06 -0500

    driver3 update

--- TestAutomation/testCasesExecutables/driver3.java
+++ TestAutomation/testCasesExecutables/driver3.java
@@ -5,12 +5,19 @@ public class driver3
 
 	public static void main(String args[])
 	{
+		String output;
+		try{
 
 		if(args.length>0)
 			System.out.println(OpenmrsUtil.containsDigit(args[0]));
 		else
 			System.out.println(OpenmrsUtil.containsDigit(null));
 
+		}
+		catch(Exception e){
+			output = e.getMessage();
+			System.out.println(output);
+		}
 	}
 
 }
