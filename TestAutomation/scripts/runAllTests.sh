3d6938b2c78c8529ff6b8acffa04b78e1746534d
deyc <deyc@g.cofc.edu>
2019-11-18 15:12:38 -0500

    Boolean Option to use Faulty Jar added

--- TestAutomation/scripts/runAllTests.sh
+++ TestAutomation/scripts/runAllTests.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+
+###########################################
+#Decides to use the standard or faulty jar#
+###########################################
+
+
+echo "Use the faulty Jar?"
+echo "Enter 'Yes' or 'yes' to use"
+echo "Pressing enter or anything else will use the standard jar."
+
+read userInput
+
+jar="false"
+
+if [ $userInput = 'Yes' ] || [ $userInput = 'yes' ]
+then
+	jar="true"
+fi
+
 #Removes report files from reports directory
 rm -rf ./reports/*.txt
 rm -rf ./reports/finalReport.html
@@ -8,8 +27,18 @@ rm -rf ./reports/finalReport.html
 #Compile all "driver" Java files#
 #################################
 
+
 cd testCasesExecutables
-javac -cp openmrs-api.jar: ./*.java
+
+
+if [ $jar = "true" ]
+then
+	javac -cp faultyMRS.jar: ./*.java
+else
+	javac -cp openmrs-api.jar: ./*.java	
+fi
+
+
 cd ..
 
 
@@ -36,7 +65,19 @@ for file in testCases/*.txt; do
 
 	#Find output of the method driver using input
 	cd testCasesExecutables
-	output=$(java -cp openmrs-api.jar: $driver $input)
+
+
+
+	if [ $jar = "true" ]
+	then
+		output=$(java -cp faultyMRS.jar: $driver $input)
+	else
+		output=$(java -cp openmrs-api.jar: $driver $input)
+	fi
+	
+
+
+
 	cd ..
 
 	#Compare output to oracle to get result (pass/fail)
